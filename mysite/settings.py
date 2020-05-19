dadc7118eac8ba6c4cdf952a3423c8a3927a5344
Shreeram Kelkar <shreeramkelkar7@gmail.com>
2020-05-19 18:42:19 +0530

    ready

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,6 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_URL = '/static/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = '/login'
